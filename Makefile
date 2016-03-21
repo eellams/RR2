ca34e0ac314ec73f28661fbb01f5dd167d479832
Eddie Ellams <e_ellams@hotmail.co.uk>
2016-03-21 17:06:53 +0000

    Added main file.

---
+++ Makefile
@@ -0,0 +1,23 @@
+CC      = g++
+CFLAGS  = -I/usr/include/boost -I/usr/include/python3.4 -Wno-unknown-pragmas
+LDFLAGS = -lpython3.4m -lboost_python3
+
+# All objects are c++ (with .cxx extension)
+OBJ = main
+
+all: config $(OBJ)
+
+config:
+	cd config && make && cd ../
+
+#%.o: %.cpp
+#	$(CC) $(CFLAGS) -o $@ $< $(LDFLAGS)
+
+main: main.cpp
+	$(CC) $(CFLAGS) -o $@ $< $(LDFLAGS)
+
+cleanconfig:
+	cd config && make clean && cd ../
+
+clean: cleanconfig
+	rm -r -f $(OBJ)
--- main.cpp
+++ main.cpp
@@ -1,6 +1,3 @@
-// based on boost python tutorial: http://www.boost.org/doc/libs/1_58_0/libs/python/doc/tutorial/doc/html/python/embedding.html
-// usage: g++ -I/usr/include/boost  -I/usr/include/python3.4 main.cpp -o main -lpython3.4m -lboost_python3
-
 #include <boost/python.hpp>
 #include <boost/python/object.hpp>
 
@@ -14,23 +11,8 @@ int main(int argc, char *argv[]) {
 
         object main_module = import("__main__");
         object main_namespace = main_module.attr("__dict__");
+        object ignore = exec("print(\"Hi!\")\n", main_namespace);
 
-        /*object ignored = exec("hello = open('hello.txt', 'w')\n"
-                              "hello.write('Hello world!')\n"
-                              "hello.close()",
-                              main_namespace);;*/
-        object test = exec("import sys; import hello;"
-                          "sys.path.append('/home/eddie/projects/RR2');", main_namespace);
-
-        object planet = exec("planet = hello.World()", main_namespace);
-                          /*"import hello;"
-                          "planet = hello.World();"
-                          "planet.set('howdy');"
-                          "print(planet.greet());", main_namespace);*/
-
-        object test2 = exec("class asdf(hello.World):\n"
-                          "\tdef greet(self):\n"
-                          "\treturn \"redefined\"\n\n", main_namespace );
    }
    catch (error_already_set const&) {
         PyErr_Print();
